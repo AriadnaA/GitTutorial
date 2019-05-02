d02594b33f0a31413a09ce63e7853884b60a6029
Kate Arapova <katearapova@gmail.com>
2019-05-02 12:57:11 +0300

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Kateryna Arapova on 02/05/2019.
+//  Copyright © 2019 Kateryna Arapova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
